680c994e1a6a8c429efcbd9cb3a6b76eb79787aa
Danjal <Danjal_berg@msn.com>
2022-04-28 11:29:47 +0200

    Cookiecutter init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Probabilistic Elastic Deformation Model from Ice Sheet Mass Change',
+    author='Danjal',
+    license='MIT',
+)
